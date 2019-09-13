7b550632130b87c33e2cd36c3a0746c7fdf34235
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-09-13 16:04:52 -0500

    15 Proyecto Final del Curso Estructura del proyecto y referencias

---
+++ Infraestructura.Data.SQLServer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructura.Data.SQLServer
+{
+    public class Class1
+    {
+    }
+}
