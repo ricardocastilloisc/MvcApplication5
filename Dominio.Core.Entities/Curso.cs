ddfe134328edb4ff13f143eee472a58f2a002ecd
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-09-13 16:35:40 -0500

    16 Proyecto Final del Curso Capa Dominio Data Annotations Capa Infraestructura

---
+++ Dominio.Core.Entities/Curso.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Dominio.Core.Entities
+{
+    class Curso
+    {
+        [DisplayName("Código del curso")]
+        public int codigo_c { get; set; }
+
+        [DisplayName("Nombre del Curso")]
+        [Required(ErrorMessage = "Nombre del curso es reuerido")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "No más de 30 caracteres")]
+        public string nombre_c { get; set; }
+
+        [DisplayName("Email del Curso")]
+        [Required(ErrorMessage = "Email del curso requerido")]
+        [StringLength(50, ErrorMessage = "No más de 50 caracteres")]
+        [EmailAddress(ErrorMessage = "Email invalido")]
+        public string correo_c { get; set; }
+
+        [DisplayName("Crédito del curso")]
+        [Required(ErrorMessage = "Número de crédito de curso es requrido")]
+        [Range(1, 6)]
+        public int credito_c { get; set; }
+    }
+}
---
+++ Infraestructura.Data.SQLServer/Conexion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Infraestructura.Data.SQLServer
+{
+    class Conexion
+    {
+        SqlConnection conexion;
+        public SqlConnection Conectar() { 
+            conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString);
+            return conexion;
+        }
+    }
+}
