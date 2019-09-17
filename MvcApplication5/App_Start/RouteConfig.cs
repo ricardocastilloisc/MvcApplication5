a26e51d9ea6c152b3b2cb76552441d0cf4ea7c0f
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-09-17 11:21:39 -0500

    23  Proyecto Final del Curso Capa Presentación Creación de Vistas II Validación con @Section Scripts

--- MvcApplication5/App_Start/RouteConfig.cs
+++ MvcApplication5/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace MvcApplication5
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Usuario", action = "Validar", id = UrlParameter.Optional }
             );
         }
     }
